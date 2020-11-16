9fc85f6544f3ddb7474372ce59e8c7add0808af5
luigi vanacore <luigi.vanacore@outlook.com>
2020-11-16 20:12:17 +0100

    added RectangleShape struct

---
+++ Core/Shapes/RectangleShape.go
@@ -0,0 +1,28 @@
+package Shapes
+
+import "luigi.vanacore/go-snake/Core/Vector2D"
+
+type RectangleShape struct {
+	size Vector2D.Vector2D
+	Shape
+}
+
+func New(size Vector2D.Vector2D) RectangleShape {
+	return RectangleShape{size: size}
+}
+
+func (rect *RectangleShape) SetSize(size Vector2D.Vector2D) {
+	rect.size = size
+}
+
+func (rect *RectangleShape) GetSize() Vector2D.Vector2D {
+	return rect.size
+}
+
+func (rect *RectangleShape) GetPointCount() uint32 {
+	return 0
+}
+
+func (rect *RectangleShape) GetPoint(index uint) Vector2D.Vector2D {
+	return Vector2D.Zero()
+}
